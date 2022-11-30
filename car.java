3675ec02eeac2a30d6390ed8b698e6168e766e98
saranyamanikandan2022 <119424959+saranyamanikandan2022@users.noreply.github.com>
2022-11-30 10:37:36 +0530

    Create car.java

---
+++ car.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class ObjectForMobile {
+public static void main(String[] args) {
+		
+		int noOfWheels=2;
+		System.out.println("noOfWheels  "+noOfWheels);
+		short  noOfMirrors=2;
+		System.out.println("noOfMirrors  "+noOfMirrors);
+		long chasisNumber=457657656655447L;
+		System.out.println("chasisNumber  "+chasisNumber);
+		String bikeName ="ROYAL ENFIELD";
+		System.out.println("bikeName "+bikeName);
+		double runningKm=567.98898;
+		System.out.println("runningKm  "+runningKm);
+		
+	}
+
+}
